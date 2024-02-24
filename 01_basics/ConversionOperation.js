6c6bfff37402df12d4452693783cd4ea608ba77e
prajajadhav <104924386+prajajadhav@users.noreply.github.com>
2024-02-24 19:29:40 +0530

    Add files via upload

---
+++ 01_basics/ConversionOperation.js
@@ -0,0 +1,40 @@
+let value = 3
+console.log(2 + 2)
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)   //2 raise to the power 2
+// console.log(2/2)
+// console.log(2%2)   //Modulus
+
+let str1 = "Hello"
+let str2 = "  Prajakta"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log( "1" + 2 + 2); //If string is on first then all will convert in string
+// console.log(1 + 2 + "2"); // If number is on first then number conversion or operation will be done first
+//console.log( 3 + 2 * 5 % 2);
+
+// console.log(+true);
+// console.log(+"");     //tricky conversion not to used
+
+
+ let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2   //this is also tricky one
+
+ let gameCounter = 100
+ gameCounter++      //++gameCounter
+ console.log(gameCounter);
+
+
+
+
+
+
