2599cb46d45efc87c1658ac834e4c2b8bbd1e195
prajajadhav <104924386+prajajadhav@users.noreply.github.com>
2024-02-24 19:32:09 +0530

    Add files via upload

---
+++ 01_basics/Strings.js
@@ -0,0 +1,14 @@
+const name = "Prajakta"
+const repoCount = 1
+
+//console.log(name + repoCount + "Value");
+ 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+//We used back ticks. It called as string interpolation.We makes placeholder to inject variables.
+
+const gameName = new String('Prajakta')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length)
