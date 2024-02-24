354df2c7a8e0cad8cf23e2a58598b19fe6f2d284
prajajadhav <104924386+prajajadhav@users.noreply.github.com>
2024-02-24 19:26:48 +0530

    Add files via upload

---
+++ 01_basics/DatatypeConversion.js
@@ -0,0 +1,39 @@
+//let score = "33abc"
+
+//let score = null
+
+//let score = true
+
+let score = "Prajakta"
+
+console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN not a number
+//true =>  1; false => 0;
+
+let isLoggedIn = "Prajakta"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+//"" => false
+// "Prajakta" => true
+
+let someNumber = 44
+let strinNumber = String(someNumber)
+//console.log(strinNumber)
+//console.log(typeof strinNumber)
+
+
+//*************Operations****************************
+
+let value = 4
+let negvalue = -value
+console.log(negvalue);
